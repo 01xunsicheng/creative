27d44db4511e20278b3e23285713440c5d0b2233
诚明小巫 <1312528339@qq.com>
2017-11-28 16:14:58 +0800

    提交

---
+++ js/globle1.js
@@ -0,0 +1,7 @@
+$(function(){
+	//头部尾部引进
+	$(".header_content").load("common/header1.html");
+	$(".footer_content").load("common/footer.html");
+	
+
+})
---
+++ 高企业/js/globle.js
@@ -0,0 +1,5 @@
+$(function(){
+	//头部尾部引进
+	$(".header_content").load("common/header.html");
+	$(".footer_content").load("common/footer.html");
+})
